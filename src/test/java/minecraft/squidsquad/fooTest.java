513eec245a25fbe6b6b14b1d0c723f2dbd05584d
Glenn A. Mulder <gmulder@sogyo.nl>
2020-12-21 18:04:04 +0100

    Deleted some white lines in fooTest.java

--- src/test/java/minecraft/squidsquad/fooTest.java
+++ src/test/java/minecraft/squidsquad/fooTest.java
@@ -1,15 +1,13 @@
 package minecraft.squidsquad;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertEquals;
 
 public class fooTest{
-
     @Test
     public void testWillPass(){
         int a = 1;
         assertEquals(1, a);
     }
-
     @Test
     public void thisTestWillFail(){
         int a = 1;
