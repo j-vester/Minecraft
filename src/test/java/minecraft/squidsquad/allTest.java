893d7073ebf89b44059770abe76d7159bd72ae62
Sabine Glotzbach <sglotzbach@sogyo.nl>
2020-12-22 15:39:27 +0100

    Trying to figure out mockito

--- src/test/java/minecraft/squidsquad/allTest.java
+++ src/test/java/minecraft/squidsquad/allTest.java
@@ -1,8 +1,11 @@
 package minecraft.squidsquad;
 import org.bukkit.util.*;
+import org.bukkit.entity.*;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class allTest{
@@ -48,6 +51,29 @@ public class allTest{
         Vector setWind = new Windspeed(3, 2);
         assertEquals(0, wind.getZ());
         assertEquals(0, setWind.getZ());
+    }
+    
+    //Instantiating a mock arrow to be used in tests
+    Arrow arrow = mock(Arrow.class);    
+
+    @Test
+    public void testArrowExists(){
+        assertNotNull(arrow);
+    }
+
+    @Test
+    public void testArrowHasNoVelocity(){
+        assertNull(arrow.getVelocity());
+    }
+
+    @Test
+    public void testArrowCanBeGivenVelocity(){
+        Vector velocity = new Vector(2, 3, 0);
+        arrow.setVelocity(velocity);
+        verify(arrow).setVelocity(velocity);
+        //Verify works, but it doesn't change the actual
+        //properties of the object:
+        assertNull(arrow.getVelocity());
     }
 
     /////////////////////
