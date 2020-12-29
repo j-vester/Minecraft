78d8696dcc0437ccc4e2398ac22254f508cd7b84
Glenn A. Mulder <gmulder@sogyo.nl>
2020-12-29 17:02:58 +0100

    Uncommented default give blade when joining

--- src/main/java/minecraft/squidsquad/ExcaliPurr.java
+++ src/main/java/minecraft/squidsquad/ExcaliPurr.java
@@ -9,13 +9,45 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ShapedRecipe;
 
-public class ExcaliPurr extends JavaPlugin{
+public class ExcaliPurr extends JavaPlugin implements Listener{
 	
 	Logger excaliPurrLogger = Bukkit.getLogger();
+	
+	@Override
+    public void onEnable(){
+		System.out.println("ExcaliPurr from AttackPlugin has been enabled!");
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+		String excaliPurr = ("ExcaliPurr");
+		ItemStack myItem = new ItemStack(Material.NETHERITE_SWORD);
+		ItemMeta im = myItem.getItemMeta();
+		im.setDisplayName(excaliPurr);
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add("This mythical blade is used to summon mythical creatures!");
+		myItem.setItemMeta(im);
+        @SuppressWarnings("deprecation")
+		ShapedRecipe hRecipe = new ShapedRecipe(myItem);
+        hRecipe.shape(
+                "#@#",
+                "!@!",
+                "#&#");
+        hRecipe.setIngredient('@', Material.IRON_INGOT);
+        hRecipe.setIngredient('!', Material.NETHERITE_INGOT);
+        hRecipe.setIngredient('#', Material.AIR);
+            Bukkit.getServer().addRecipe(hRecipe);
+	}
+	
+	@Override
+    public void onDisable(){
+            System.out.println("ExcaliPurr from AttackPlugin has been disabled!");
+    }
 	
 	public static void giveCatBlade(Player player) {
 		String excaliPurr = ("ExcaliPurr");
