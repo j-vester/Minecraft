e61f492cdcb39da1613df5e812867b98a35715b2
Christelle Klein <christelleklein064@gmail.com>
2020-12-29 13:34:20 +0100

    per ongeluk dingetjes veranderd terwijl in master branch

--- src/main/java/minecraft/squidsquad/MyCommandExecutor.java
+++ src/main/java/minecraft/squidsquad/MyCommandExecutor.java
@@ -55,7 +55,7 @@ public class MyCommandExecutor implements CommandExecutor {
             Player p = (Player)sender;
             Location loc = p.getLocation();
             World world = p.getWorld();
-            Creeper creeper = world.spawnEntity(loc, Creeper.class);
+            //Creeper creeper = world.spawnEntity(loc, Creeper.class);
             //p.getWorld().spawnEntity(loc, EntityType.DOLPHIN);
         }
         return true;
